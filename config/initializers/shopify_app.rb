36af8911f367b9236d49006f80ae97e5af313cfc
ujjwal mishra <ujjwalmishra7@gmail.com>
2015-10-31 11:47:49 +0530

    generate shopify app n

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,6 @@ ShopifyApp.configure do |config|
   config.api_key = "5aafccc43748a2e614ccb00b4b46533e"
   config.secret = "dc059acae8beef7d54c509dd74bd17a4"
   config.redirect_uri = "https://spotdiscount.herokuapp.com/authorize/shopify/callback"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_products"
   config.embedded_app = true
 end
